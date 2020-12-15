288846b6995b08522925ad30948d8ec1a169c1c9
Jae Perris <jae.perris@gmail.com>
2020-12-15 16:29:22 +0100

    Restrict file load filetypes

--- src/LoadButton.tsx
+++ src/LoadButton.tsx
@@ -4,21 +4,25 @@ export const LoadButton = ({
   setLoadedImage: (image: HTMLImageElement) => void;
 }) => {
   return (
-    <input
-      onChange={async (event) => {
-        const imagePromise = new Promise<HTMLImageElement>(
-          (resolve, reject) => {
-            const imageUrl = URL.createObjectURL(event.target.files![0]);
-            const image = new Image();
-            image.src = imageUrl;
-            image.onload = () => resolve(image);
-          }
-        );
-        const image = await imagePromise;
+    <label>
+      Upload Image
+      <input
+        onChange={async (event) => {
+          const imagePromise = new Promise<HTMLImageElement>(
+            (resolve, reject) => {
+              const imageUrl = URL.createObjectURL(event.target.files![0]);
+              const image = new Image();
+              image.src = imageUrl;
+              image.onload = () => resolve(image);
+            }
+          );
+          const image = await imagePromise;
 
-        setLoadedImage(image);
-      }}
-      type="file"
-    />
+          setLoadedImage(image);
+        }}
+        type="file"
+        accept="image/*"
+      />
+    </label>
   );
 };
