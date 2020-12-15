62363fca0cd1d5c1259458bffc97e439c3002c22
Jae Perris <jae.perris@gmail.com>
2020-12-15 19:07:12 +0100

    Fix redrawing of canvas

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -28,16 +28,20 @@ export const Grid = ({
 
     // Horizontal lines
     const yIncrement = canvas.height / pixelDimensions;
-    console.log(yIncrement);
     for (let yIndex = 0; yIndex < pixelDimensions; yIndex++) {
       context.moveTo(0, Math.round(yIndex * yIncrement));
       context.lineTo(canvas.width, Math.round(yIndex * yIncrement));
       context.stroke();
     }
+
+    return () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    };
   });
   return (
     <canvas
       ref={canvasRef}
+      key={pixelDimensions}
       className="Grid"
       width={rootCanvas.clientWidth}
       height={rootCanvas.clientHeight}
