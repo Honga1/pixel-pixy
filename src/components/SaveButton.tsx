fba5116a1abed6968a334da8376241dc35b3a1ed
Jae Perris <jae.perris@gmail.com>
2020-12-18 13:36:07 +0100

    Fix page overflowing

--- src/components/SaveButton.tsx
+++ src/components/SaveButton.tsx
@@ -1,29 +1,42 @@
-import { Anchor } from "grommet";
+/* eslint-disable jsx-a11y/anchor-has-content */
+import { Button } from "grommet";
 import { Share } from "grommet-icons";
+import { useRef } from "react";
 
 export const SaveButton = ({ canvas }: { canvas: HTMLCanvasElement }) => {
+  const anchorRef = useRef<HTMLAnchorElement>(null);
   return (
-    <Anchor
-      icon={<Share />}
-      onTouchEnd={(event) => {
-        const fullScaleCanvas = document.createElement("canvas");
-        fullScaleCanvas.width = window.innerWidth;
-        fullScaleCanvas.height = window.innerWidth;
-        const context = fullScaleCanvas.getContext("2d")!;
-        context.imageSmoothingEnabled = false;
-        context.drawImage(
-          canvas,
-          0,
-          0,
-          fullScaleCanvas.width,
-          fullScaleCanvas.height
-        );
+    <>
+      <Button
+        icon={<Share />}
+        onClick={(event) => {
+          const fullScaleCanvas = document.createElement("canvas");
+          fullScaleCanvas.width = window.innerWidth;
+          fullScaleCanvas.height = window.innerWidth;
+          const context = fullScaleCanvas.getContext("2d")!;
+          context.imageSmoothingEnabled = false;
+          context.drawImage(
+            canvas,
+            0,
+            0,
+            fullScaleCanvas.width,
+            fullScaleCanvas.height
+          );
 
-        const image = fullScaleCanvas.toDataURL("image/png");
-        (event.target as EventTarget & HTMLAnchorElement).href = image;
-      }}
-      href="/"
-      download="my-pixel-art.png"
-    ></Anchor>
+          const image = fullScaleCanvas.toDataURL("image/png");
+
+          if (anchorRef.current) {
+            anchorRef.current.href = image;
+            anchorRef.current.click();
+          }
+        }}
+      />
+      <a
+        ref={anchorRef}
+        href="/"
+        style={{ display: "none" }}
+        download="my-pixel-art.png"
+      />
+    </>
   );
 };
