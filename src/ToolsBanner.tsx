9384645f523efb40fc02d441eff1ae9642d50998
Jae Perris <jae.perris@gmail.com>
2020-12-26 16:04:42 +0100

    Make tools banner constant height

--- src/ToolsBanner.tsx
+++ src/ToolsBanner.tsx
@@ -41,68 +41,70 @@ export const ToolsBanner = ({
   color: RGBColor;
 }) => {
   return (
-    <Grid
-      fill
-      columns={["auto", "auto"]}
-      rows={["flex", "flex"]}
-      areas={[
-        { name: "left-top", start: [0, 0], end: [0, 0] },
-        { name: "left-bot", start: [0, 1], end: [0, 1] },
-        { name: "right-top", start: [1, 0], end: [1, 0] },
-        { name: "right-bot", start: [1, 1], end: [1, 1] },
-      ]}
-    >
-      <Box gridArea="left-top" direction="row">
-        <HighlightableButton
-          primary
-          onClick={() => onBrushChange("paint")}
-          isHighlighted={brush === "paint"}
-          icon={<Brush />}
-          color={color.toHex()}
-        />
-        <HighlightableButton
-          onClick={() => onBrushChange("eraser")}
-          isHighlighted={brush === "eraser"}
-          icon={<Erase />}
-        />
-        <HighlightableButton
-          primary
-          onClick={() => onBrushChange("fill")}
-          isHighlighted={brush === "fill"}
-          icon={<Paint />}
-          color={color.toHex()}
-        />
-        <Button onClick={onPaletteButtonClick} icon={<PaletteIcon />} />
-        <HighlightableButton
-          onClick={() => onBrushChange("dropper")}
-          isHighlighted={brush === "dropper"}
-          icon={<DropperIcon />}
-        />
-      </Box>
-      <Box gridArea="left-bot" direction="row">
-        <HighlightableButton
-          icon={<History />}
-          onClick={() => onPickerModeClick("history")}
-          isHighlighted={pickerMode === "history"}
-        />
-        <HighlightableButton
-          icon={<Pin />}
-          onClick={() => onPickerModeClick("pinned")}
-          isHighlighted={pickerMode === "pinned"}
-        />
-      </Box>
-      <Box gridArea="right-top" direction="row" justify="end">
-        <Button icon={<Undo />} onClick={onUndoClick} />
-        <Button icon={<Redo />} onClick={onRedoClick} />
-      </Box>
-      <Box gridArea="right-bot" direction="row" justify="end">
-        <Button onClick={onTrashClick} icon={<Trash />} />
-        <HighlightableButton
-          onClick={onGridButtonClick}
-          isHighlighted={isGridShown}
-          icon={<GridIcon />}
-        />
-      </Box>
-    </Grid>
+    <Box height="xsmall">
+      <Grid
+        fill
+        columns={["auto", "auto"]}
+        rows={["flex", "flex"]}
+        areas={[
+          { name: "left-top", start: [0, 0], end: [0, 0] },
+          { name: "left-bot", start: [0, 1], end: [0, 1] },
+          { name: "right-top", start: [1, 0], end: [1, 0] },
+          { name: "right-bot", start: [1, 1], end: [1, 1] },
+        ]}
+      >
+        <Box gridArea="left-top" direction="row">
+          <HighlightableButton
+            primary
+            onClick={() => onBrushChange("paint")}
+            isHighlighted={brush === "paint"}
+            icon={<Brush />}
+            color={color.toHex()}
+          />
+          <HighlightableButton
+            onClick={() => onBrushChange("eraser")}
+            isHighlighted={brush === "eraser"}
+            icon={<Erase />}
+          />
+          <HighlightableButton
+            primary
+            onClick={() => onBrushChange("fill")}
+            isHighlighted={brush === "fill"}
+            icon={<Paint />}
+            color={color.toHex()}
+          />
+          <Button onClick={onPaletteButtonClick} icon={<PaletteIcon />} />
+          <HighlightableButton
+            onClick={() => onBrushChange("dropper")}
+            isHighlighted={brush === "dropper"}
+            icon={<DropperIcon />}
+          />
+        </Box>
+        <Box gridArea="left-bot" direction="row">
+          <HighlightableButton
+            icon={<History />}
+            onClick={() => onPickerModeClick("history")}
+            isHighlighted={pickerMode === "history"}
+          />
+          <HighlightableButton
+            icon={<Pin />}
+            onClick={() => onPickerModeClick("pinned")}
+            isHighlighted={pickerMode === "pinned"}
+          />
+        </Box>
+        <Box gridArea="right-top" direction="row" justify="end">
+          <Button icon={<Undo />} onClick={onUndoClick} />
+          <Button icon={<Redo />} onClick={onRedoClick} />
+        </Box>
+        <Box gridArea="right-bot" direction="row" justify="end">
+          <Button onClick={onTrashClick} icon={<Trash />} />
+          <HighlightableButton
+            onClick={onGridButtonClick}
+            isHighlighted={isGridShown}
+            icon={<GridIcon />}
+          />
+        </Box>
+      </Grid>
+    </Box>
   );
 };
